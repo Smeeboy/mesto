3941138a25a4b02268eff495c1ac0cd42eb06a47
Smeeboy <Dreddi91@yandex.rugit config --listgit config --global user.name Smeeboygit config --global user.email Dreddi91@yandex>
2021-09-19 13:24:08 +0300

    new project

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+const popupEl = document.querySelector('.popup');
+const popupCloseButtonEl = document.querySelector('.popup__close-button');
+const leadTitleEl = document.querySelector('.form__button');
+const profileTitle = document.querySelector('.profile__title');
+const profileText = document.querySelector('.profile__text');
+
+const formElement = popupEl.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.form__input--name--title');
+const jobInput = formElement.querySelector('.form__input--name--subtitle');
+
+function openPopup() {
+    popupEl.classList.add('popup_opened');
+    setPopupInputValue();
+}
+
+function closePopup (evt) {
+    const closeBtn = evt.target;
+    console.log (closeBtn);
+
+    if (closeBtn.classList.contains ('popup__close-button')) {
+        closeBtn.closest ('.popup').classList.remove('popup_opened');
+    }
+}
+
+popupCloseButtonEl.addEventListener('click', closePopup)
+
+function setPopupInputValue() {
+    nameInput.value = profileTitle.textContent.trim();
+    jobInput.value = profileText.textContent.trim();
+}
+
+function setProfileInputValue() {
+    profileTitle.innerHTML = nameInput.value;
+    profileText.innerHTML = jobInput.value;
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    setProfileInputValue();
+    popupEl.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
