54384ec4a7d62172b9412bf95ca085b27b17f6a8
Smeeboy <Dreddi91@yandex.rugit config --listgit config --global user.name Smeeboygit config --global user.email Dreddi91@yandex>
2021-09-22 18:38:42 +0300

    corrected js, css, html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const popupEl = document.querySelector('.popup');
 const popupCloseButtonEl = document.querySelector('.popup__close-button');
+const pageEl = document.querySelector('.page');
 const leadTitleEl = document.querySelector('.form__button');
 const profileTitle = document.querySelector('.profile__title');
 const profileText = document.querySelector('.profile__text');
@@ -8,40 +9,36 @@ const formElement = popupEl.querySelector('.popup__form');
 const nameInput = formElement.querySelector('.form__input-name-title');
 const jobInput = formElement.querySelector('.form__input-name-subtitle');
 
-function openPopup() {
-    popupEl.classList.add('popup_opened');
-    setPopupInputValue();
-}
-
-function closePopup (evt) {
-    const closeBtn = evt.target;
-    console.log (closeBtn);
-
-    if (closeBtn.classList.contains ('popup__close-button')) {
-        closeBtn.closest ('.popup').classList.remove('popup_opened');
-    }
-}
-
-popupCloseButtonEl.addEventListener('click', closePopup)
-
 function setPopupInputValue() {
     nameInput.value = profileTitle.textContent.trim();
     jobInput.value = profileText.textContent.trim();
 }
 
-function setProfileInputValue() {
-    profileTitle.innerHTML = nameInput.value;
-    profileText.innerHTML = jobInput.value;
+function openPopup() {
+    popupEl.classList.add('popup_opened');
+    pageEl.classList.add('page_openpopup');
+    setPopupInputValue();
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    setProfileInputValue();
+function closePopup() {
     popupEl.classList.remove('popup_opened');
+    pageEl.classList.remove('page_openpopup');
 }
 
+function setProfileInputValue() {
+    profileTitle.textContent = nameInput.value;
+    profileText.textContent = jobInput.value;
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    setProfileInputValue();
+    closePopup ();
+}
+
+popupCloseButtonEl.addEventListener('click', closePopup)
 formElement.addEventListener('submit', formSubmitHandler);
 // popupEl.addEventListener('click', function (evt) {
-    
+
 //     popupEl.classList.remove('popup_opened')
 // });
