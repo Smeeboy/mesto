1c7a7c4d58bbe93c40b44cb39ca63d973c4ba675
Smeeboy <Dreddi91@yandex.rugit config --listgit config --global user.name Smeeboygit config --global user.email Dreddi91@yandex>
2021-10-01 19:15:19 +0300

    const instead of let

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 const initialCards = [
     {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
     },
     {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
     },
     {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
     },
     {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
     },
     {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
-  ];
-  
+];
+
 const templateCardElement = document.querySelector('#card').content;
 const cardsElement = document.querySelector('.cards');
 const addCardForm = document.querySelector('#AddCardForm');
@@ -35,27 +35,27 @@ const popupAddCard = document.querySelector('.popup_add_card');
 const popupCardView = document.querySelector('.popup_card_view');
 const popupProfileEdit = document.querySelector('.popup_profile_edit');
 
-function showImage (link, title) {
+function showImage(link, title) {
     imageElement.src = link;
     imageTitle.textContent = title;
-    imageTitle.alt= title;
+    imageTitle.alt = title;
     openPopup(popupCardView);
 };
- 
+
 
 
 function addCardEvents(cardElement) {
-    let deleteButton = cardElement.querySelector('.button_action_delete');
-    let likeButton = cardElement.querySelector('.card__button-like');
-    let cardImage = cardElement.querySelector('.card__image');
-    let cardTitle = cardElement.querySelector('.card__title').textContent;
-    deleteButton.addEventListener('click', function(evt){
+    const deleteButton = cardElement.querySelector('.button_action_delete');
+    const likeButton = cardElement.querySelector('.card__button-like');
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title').textContent;
+    deleteButton.addEventListener('click', function (evt) {
         deleteButton.closest('.card').remove()
     });
-    likeButton.addEventListener('click', function(evt){
+    likeButton.addEventListener('click', function (evt) {
         likeButton.classList.toggle('card__button-like_active')
     });
-    cardImage.addEventListener('click', function(evt){
+    cardImage.addEventListener('click', function (evt) {
         showImage(cardImage.src, cardTitle);
     });
 };
@@ -64,34 +64,34 @@ function createCard(link, name) {
     const cardElement = templateCardElement.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__image').src = link;
     cardElement.querySelector('.card__image').alt = name;
-    cardElement.querySelector('.card__title').textContent =  name;
+    cardElement.querySelector('.card__title').textContent = name;
     addCardEvents(cardElement);
     return cardElement
-} 
+}
 
 function addCard(link, name) {
     const cardElement = createCard(link, name);
     cardsElement.prepend(cardElement);
 }
 
-initialCards.forEach(function(item){
+initialCards.forEach(function (item) {
     addCard(item.link, item.name);
 });
 
 
-addCardForm.addEventListener('submit', function(evt){
+addCardForm.addEventListener('submit', function (evt) {
     evt.preventDefault()
     const formInputText = document.querySelector('.form__input_name_text');
     const formInputLink = document.querySelector('.form__input_name_link');
     const title = formInputText.value;
-    const link = formInputLink.value;  
+    const link = formInputLink.value;
     addCard(link, title);
     formInputText.value = '';
     formInputLink.value = '';
     closePopup(evt.target.closest('.popup'))
 });
 
-const allCards =  document.querySelectorAll('.card');
+const allCards = document.querySelectorAll('.card');
 const profilePopup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.button_action_edit');
 const popupAddCardButton = document.querySelector('.button_action_add');
@@ -127,22 +127,22 @@ function setProfileInputValue() {
 function formSubmitHandler(evt) {
     evt.preventDefault();
     setProfileInputValue();
-    closePopup (evt.target.closest('.popup'));
+    closePopup(evt.target.closest('.popup'));
 }
 
-popupOpenButton.addEventListener('click', function (){
+popupOpenButton.addEventListener('click', function () {
     openPopup(popupProfileEdit);
-    setPopupInputValue ();
+    setPopupInputValue();
 });
 
-popupAddCardButton.addEventListener('click', function (){
+popupAddCardButton.addEventListener('click', function () {
     openPopup(popupAddCard);
 });
 
 
-popupCloseButtonEls.forEach(function (item){
-    item.addEventListener('click', function(evt){
-       closePopup(evt.target.closest('.popup'));
+popupCloseButtonEls.forEach(function (item) {
+    item.addEventListener('click', function (evt) {
+        closePopup(evt.target.closest('.popup'));
     });
 });
 formElement.addEventListener('submit', formSubmitHandler);
