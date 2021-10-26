10ca3b0ccdc4b2f72d82c0af05dd85ff2991808d
Smeeboy <Dreddi91@yandex.rugit config --listgit config --global user.name Smeeboygit config --global user.email Dreddi91@yandex>
2021-10-26 21:36:03 +0300

    edited card, formvalidator paths

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import Card from 'Card.js';
-import FormValidator from 'FormValidator.js';
+import Card from 'js/FormValidator.js';
+import FormValidator from 'js/FormValidator.js';
 
 const initialCards = [
     {
